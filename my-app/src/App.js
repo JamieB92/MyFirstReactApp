b0ebf9db6bd3f2962610124c064f4e8e044a357a
JamieB92 <j.bonner6992@gmail.com>
2023-10-13 20:25:47 +0000

    class components part lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import StatefulGreeting from './components/StatefulGreeting';
 
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <FunctionalGreetingWithProps greeting="Nice to meet you, I see you are " name="Jamie" age="31 years old." />
+      <StatefulGreeting greeting="Im a stateful class component" />
     </div>
   );
 }
