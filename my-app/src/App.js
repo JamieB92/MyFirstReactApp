9bf94d7fda78da687444ee0ff1ad40fc318ce5c4
JamieB92 <j.bonner6992@gmail.com>
2023-10-14 15:48:59 +0000

    update the state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import StatefulGreeting from './components/StatefulGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StateFulGreetingWithPrevState';
+
 
 function App() {
   return (
     <div className="App">
        {/* <FunctionalGreetingWithProps greeting="Nice to meet you, I see you are " name="Jamie" age="31 years old." />  */}
       {/* <StatefulGreeting greeting="Im a stateful class component" /> */}
-      <StatefulGreetingWithCallback />
+      {/* <StatefulGreetingWithCallback /> */}
+      <StatefulGreetingWithPrevState />
     </div>
   );
 }
