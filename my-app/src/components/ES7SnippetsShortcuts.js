7cec2d7fcdd874d22e8506185abe9a5f375bdb11
JamieB92 <j.bonner6992@gmail.com>
2023-10-14 16:51:08 +0000

    add ES7 shortcut file

---
+++ my-app/src/components/ES7SnippetsShortcuts.js
@@ -0,0 +1,60 @@
+// rfce shortcut creates function
+
+// import React from 'react'
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// rafce shortcute - create function as an arror function
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+
+// rce shortcut - create class funtion 
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+
+
+// rcon - creates constructor
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      
+//   }
+// }
+
+
+// clg - consoles to the log
+// console.log(first)
+
+// clo - log a obejct with a string
+
+// console.log('first', first)
