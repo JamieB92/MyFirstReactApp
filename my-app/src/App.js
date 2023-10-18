1406872c2256b330f6c8d2200951d37ae8f5ac2b
JamieB92 <j.bonner6992@gmail.com>
2023-10-18 20:42:06 +0000

    learn how to add a form in react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ import RenderingLists from './components/RenderingLists';
 import LifeCyclesCDM from './components/LifeCyclesCDM';
 import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from './components/LifeCyclesCWU';
-
+import ControlledForm from './components/ControlledForm';
 
 function App() {
   return (
@@ -34,7 +34,8 @@ function App() {
       {/* <RenderingLists /> */}
       {/* <LifeCyclesCDM /> */}
       {/* <LifeCyclesCDU /> */}
-      <LifeCyclesCWU />
+      {/* <LifeCyclesCWU /> */}
+      <ControlledForm />
     </div>
   );
 }
