a83868c072ed5942b2824674aad16382a81c1d34
JamieB92 <j.bonner6992@gmail.com>
2023-10-21 14:13:38 +0000

    learn how to run useEffect only once by using an empty array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -50,7 +50,7 @@ function App() {
       {/* <UseStateWithArrays /> */}
       {/* <HooksCounter /> */}
       {/* <UseStateWithObjects /> */}
-      <ClassCounter />
+      {/* <ClassCounter /> */}
       <UseEffectCounter />
     </div>
   );
--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,19 +2,29 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+
     useEffect(() => {
-        document.title = count
-        // Always specify explictiyt that you want the effect depends on 
+        console.log('count 1 effect');
+        document.title = count;
     }, [count]);
-  return (
-    <div>
-        <button onClick={
-            () => setCount(count => count + 1)
-        }>
-            Increment Count ({count})
-        </button>
-    </div>
-  )
+
+    useEffect(()=>{
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+    }, []);
+    return (
+        <div>
+            <button onClick={
+                () => setCount(count => count + 1)
+            }>
+                Increment Count ({count})
+            </button>
+            <h2>Time is {time}</h2>
+        </div>
+    )
 }
 
 export default UseEffectCounter
